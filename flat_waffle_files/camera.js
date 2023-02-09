5fb1ecbdf4e89945edc37dea63084fe19266387d
Curby-lee Williams <curbris@gmail.com>
2023-02-09 10:04:11 +0200

    Updating project #2 - new repo for flat waffle

--- flat_waffle_files/camera.js
+++
@@ -1,5 +0,0 @@
-//Camera Setting
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 20;
-
-export{ camera };
--- flat_waffle_files/renderer.js
+++
@@ -1,5 +0,0 @@
-//Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-export{ renderer };
